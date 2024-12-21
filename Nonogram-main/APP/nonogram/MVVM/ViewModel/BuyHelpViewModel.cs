c7d512dcd0e923714a2eaa2f65f02035c0db3c4a
szabopatrik0411 <szabopatrik0411@gmail.com>
2024-12-21 14:20:13 +0100

    no message

--- Nonogram-main/APP/nonogram/MVVM/ViewModel/BuyHelpViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace nonogram.MVVM.ViewModel
-{
-    public class BuyHelpViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-    }
-}
